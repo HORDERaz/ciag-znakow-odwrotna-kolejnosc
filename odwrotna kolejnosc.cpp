9c434690cad5a8abddfd45dee2d28be930266a64
HORDERaz <85604906+HORDERaz@users.noreply.github.com>
2021-06-09 08:29:10 +0200

    Add files via upload

---
+++ odwrotna kolejnosc.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+int main() {
+		
+		string zdanie="Ala ma kota";
+		cout<<zdanie.length()<<endl;
+		cout<<"zdanie w odwrotnej kolejnosci";
+		for(int i=zdanie.length()-1;i>0;i--)
+		{
+			cout<<endl<<zdanie[i];
+		}
+		
+		for(int i=0;zdanie.length();i++)
+		{
+			cout<<zdanie[i]<<endl;
+		}
+	
+	for (int i=0;zdanie.length;i++)
+	{
+		if(i%2==1)
+		{
+			cout<<endl<<zdanie[i]
+		}
+	}
+	
+	
+ cout<<endl<<(char)98;
+ cout<<endl<<int('a');
+
+	
+	
+	
+	return 0;
+}	
